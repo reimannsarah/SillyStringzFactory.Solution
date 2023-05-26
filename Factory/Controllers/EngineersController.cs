04e53b64194b084f0fef7c4de048610dca05f1c5
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-26 12:25:11 -0700

    add model state validation to controller for create function

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -30,9 +30,16 @@ namespace Factory.Controllers;
     [HttpPost]
     public ActionResult Create(Engineer engineer)
     {
+        if (!ModelState.IsValid)
+        {
+          return View(engineer);
+        }
+        else
+        {
         _db.Engineers.Add(engineer);
         _db.SaveChanges();
         return RedirectToAction("Index");
+        }
     }
 
     public ActionResult AddMachine(int id)
