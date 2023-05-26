eabe8fd309f8d9e7fa0ca14426b107cb4bf00a60
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-26 12:45:12 -0700

    add splashpage to list all Engineers and Machines on the Home page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,14 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers;
 
   public class HomeController : Controller
   {
+    private readonly SSFactoryContext _db;
+
+    public HomeController(SSFactoryContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
+      List<Engineer> engineers = _db.Engineers.ToList();
+      List<Machine> machines = _db.Machines.ToList();
+      ViewBag.engineers = engineers;
+      ViewBag.machines = machines;
       return View();
     }
   }
