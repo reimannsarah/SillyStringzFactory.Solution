6442bdac4791f9c097cabd32af2ef8db90715feb
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-26 09:39:46 -0700

    add create functionality

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
 using System.Collections.Generic;
@@ -17,5 +19,19 @@ namespace Factory.Controllers;
     {
       List<Engineer> model = _db.Engineers.ToList();
       return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      // ViewBag.machineList = _db.Machines.ToList();
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Engineer engineer)
+    {
+        _db.Engineers.Add(engineer);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
     }
   }
