a05fe5f87687bdef71442d00b5f73433bddac034
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-26 08:45:51 -0700

    add home controller

---
+++ Factory/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+
+namespace Factory.Controllers;
+
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
