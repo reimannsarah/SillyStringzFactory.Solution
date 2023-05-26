83c0833dca7a186e1f32f1e518829c400a2bba6c
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-26 12:46:11 -0700

    add error message for empty text box

--- Factory/Models/Machines.cs
+++ Factory/Models/Machines.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models;
 
 public class Machine
 {
   public int MachineId { get; set; }
+  [Required(ErrorMessage="Please Enter a Name")]
   public string Name { get; set; }
   public List<EngMachine> JoinEntities { get; }
 }
